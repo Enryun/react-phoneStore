55d7eb8d37aae440470ede57b99b8b31ec455356
Duong Thang <duongthang@Duongs-iMac.net.fpt>
2019-03-11 16:36:28 +0700

    first commit

---
+++ src/Components/Button.js
@@ -0,0 +1,22 @@
+// Styled Component, no need external Css
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button `
+  text-transform: capitalize;
+  font-size: 1.4rem;
+  background: transparent;
+  border: 0.05rem solid var(--lightBlue);
+  border-color: ${props => props.cart? "var(--mainYellow)":"var(--lightBlue)"};
+  border-radius: 0.5rem;
+  cursor: pointer;
+  color: ${props => props.cart? "var(--mainYellow)":"var(--lightBlue)"};
+  transition: all 0.5s ease-in-out;
+  &:hover{
+    background: ${props => props.cart? "var(--mainYellow)":"var(--lightBlue)"};
+    color:var(--mainBlue);
+  }
+  &:focus {
+    outline: none;
+  }
+
+`
